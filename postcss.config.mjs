10569153590c219abdd6c493ba166d0bbd9ffd0e
daley21233 <daleycarolyn900@gmail.com>
2026-07-25 16:10:10 -0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,11 @@
 // postcss.config.mjs
 
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
     '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-}
+};
+
+export default config;
